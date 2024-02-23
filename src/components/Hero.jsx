84a1fbc49c61b4fcb4c2383df23c5075166c595f
swaat <swaatiza@hotmail.co.uk>
2024-02-23 16:30:26 +0100

    updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import styles from "../style";
 import { discount, robot } from "../assets";
 import GetStarted from "./GetStarted";
+import Log from "../assets/log.jpg";
 
 const Hero = () => {
   return (
@@ -33,7 +34,7 @@ const Hero = () => {
       </div>
 
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-        {/* <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" /> */}
+        <img src={Log} alt="" className="w-[100%] h-[100%] relative z-[5]" />
 
         {/* gradient start */}
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
